f2897a76a3f03ebdac32bfdcca4f14ad241374cb
ramuyk <muynarsk.rafael@gmail.com>
2025-09-08 04:05:19 +0000

    feat(alias): add new aliases for various tools and workflows

--- alias.el
+++ alias.el
@@ -1,6 +1,6 @@
 ;;; alias.el -*- lexical-binding: t; -*-
 
-;; my
+;;* my
   ;;* aliases
 (a/defalias alias/refresh-docstrings a/alias-refresh-docstrings alias)
 
@@ -15,14 +15,21 @@
 (a/defalias git/remote-add magit-remote-add magit)
 (a/defalias git/commit-amend magit-commit-amend magit)
 
+  ;;* sudo
+(a/defalias sudo/find-alternate-file find-alternate-file sudo)
+
+  ;;* cd
+(a/defalias cd/consult-shortcuts a/consult-shortcuts cd)
+
 ;;* main
   ;;* avy
-  ;;* company
-(a/defalias company/yasnippet company-yasnippet company)
-
+  ;;* claude-code
+  ;;* corfu
   ;;* consult
   ;;* copilot
-(a/defalias copilot/login copilot-login dired)
+(a/defalias copilot/login copilot-login copilot)
+(a/defalias copilot/restart-and-diagnose copilot-diagnose copilot)
+
 
   ;;* dired
 (a/defalias dired/do-async-shell-command dired-do-async-shell-command dired)
@@ -31,6 +38,8 @@
 (a/defalias dired/do-kill-lines dired-do-kill-lines dired)
 (a/defalias dired/peep-kill-buffers-without-window peep-dired-kill-buffers-without-window dired)
 (a/defalias dired/toggle-marks dired-toggle-marks dired)
+(a/defalias dired/mark-files-containing-regexp dired-mark-files-containing-regexp dired)
+(a/defalias dired/diff-split-or-current dired-diff dired)
 
   ;;* dirvish
 (a/defalias dirvish/narrow dirvish-narrow dired)
@@ -40,6 +49,12 @@
   ;;* gptel
 (a/defalias gptel/add gptel-add gptel)
 (a/defalias gptel/menu gptel-menu gptel)
+(a/defalias gptel/ask-visual-content a/gptel-ask-visual-content gptel)
+(a/defalias gptel/rewrite-region a/gptel-rewrite-region gptel)
+(a/defalias gptel/search-rewrite-history a/gptel-search-rewrite-history gptel)
+(a/defalias gptel/edit-rewrite-history a/gptel-edit-rewrite-history gptel)
+
+
 
   ;;* lsp
 (a/defalias lsp/rename lsp-rename lsp)
@@ -55,6 +70,8 @@
 (a/defalias lsp/consult-diagnostics consult-lsp-diagnostics lsp)
 (a/defalias lsp/consult-symbols consult-lsp-symbols lsp)
 (a/defalias lsp/consult-file-symbols consult-lsp-file-symbols lsp)
+(a/defalias lsp/debug-python-env a/debug-python-lsp-environment lsp)
+(a/defalias lsp/restart-python a/restart-python-lsp lsp)
 
   ;;* kmacro
 (a/defalias macro/consult-kmacro consult-kmacro macro)
@@ -68,6 +85,7 @@
 (a/defalias magit/log-buffer-file magit-log-buffer-file magit)
 (a/defalias magit/log-current magit-log-current magit)
 (a/defalias magit/blame magit-blame magit)
+(a/defalias magit/commit-amend magit-commit-amend magit)
 
   ;;* org
 (a/defalias org/goto org-goto org)
@@ -78,6 +96,7 @@
   ;;* projectile
 (a/defalias projectile/invalidate-cache projectile-invalidate-cache projectile)
 
+
   ;;* vertico
   ;;* vterm
 (a/defalias vterm/beginning-of-line vterm-beginning-of-line vterm)
@@ -85,8 +104,11 @@
   ;;* tramp
 (a/defalias tramp/cleanup-all-buffers tramp-cleanup-all-buffers tramp)
 (a/defalias tramp/cleanup-all-connections tramp-cleanup-all-connections tramp)
+;;(a/defalias tramp/cleanup-this-connection tramp-cleanup-this-connection tramp)
 
   ;;* yasnippet
+(a/defalias yasnippet/consult consult-yasnippet company)
+
 ;;* secondary
   ;;* bookmark
   ;;* centaur-tabs
@@ -94,7 +116,9 @@
 (a/defalias tab/kill-other-buffers-in-current-group centaur-tabs-kill-other-buffers-in-current-group centaur-tabs)
 (a/defalias tab/kill-match-buffers-in-current-group centaur-tabs-kill-match-buffers-in-current-group centaur-tabs)
 (a/defalias tab/refresh a/centaur-tabs-refresh centaur-tabs)
-
+(a/defalias tab/copy-path-to-clipboard centaur-tabs--copy-file-name-to-clipboard centaur-tabs)
+(a/defalias tab/new-empty-buffer +default/new-buffer centaur-tabs)
+(a/defalias tab/rename-buffer rename-buffer centaur-tabs)
 
   ;;* eshell
   ;;* embark
@@ -103,6 +127,11 @@
   ;;* flycheck
 (a/defalias flycheck/mode flycheck-mode flycheck)
 (a/defalias flycheck/consult consult-flycheck flycheck)
+(a/defalias flycheck/lsp-ui-list lsp-ui-flycheck-list flycheck)
+
+
+  ;;* hacks
+(a/defalias hack/save-buffer-to-tmp a/save-buffer-to-tmp hack)
 
   ;;* help
 (a/defalias help/at-point helpful-at-point help)
@@ -117,6 +146,12 @@
 (a/defalias imenu/consult-multi consult-imenu-multi imenu)
 (a/defalias imenu/lsp-ui-imenu lsp-ui-imenu imenu)
 (a/defalias imenu/lsp-ui-imenu--refresh lsp-ui-imenu--refresh imenu)
+
+  ;;* lookup
+(a/defalias lookup/references +lookup/references lookup)
+
+  ;;* occur
+(a/defalias occur/buffer occur lookup)
 
   ;;* org-babel
   ;;* org-capture
@@ -138,6 +173,9 @@
 (a/defalias swiper/vertico-search-symbol-at-point +vertico/search-symbol-at-point help)
 
   ;;* treemacs
+(a/defalias treemacs/collapse-all-projects treemacs-collapse-all-projects treemacs)
+
+  ;;* treesit
   ;;* which-key
   ;;* workspaces
 (a/defalias workspace/delete +workspace/delete workspaces)
@@ -148,6 +186,11 @@
 (a/defalias workspace/restore-last-session +workspace/restore-last-session workspaces)
 (a/defalias workspace/save +workspace/save workspaces)
 (a/defalias workspace/switch-to +workspace/switch-to workspaces)
+
+  ;;* window
+(a/defalias window/swap-states window-swap-states workspaces)
+  ;;* tailwindcss
+(a/defalias tailwindcss/sort lsp-tailwindcss-rustywind workspaces)
 
   ;;* undo
 (a/defalias undo/tree-visualize undo-tree-visualize workspaces)
@@ -167,11 +210,13 @@
 
   ;;* jupyter
 (a/defalias jupyter/eval-buffer jupyter-eval-buffer jupyter)
-(a/defalias jupyter/run-repl jupyter-run-repl jupyter)
+;;(a/defalias jupyter/run-repl jupyter-run-repl jupyter)
 (a/defalias jupyter/repl-associate-buffer jupyter-repl-associate-buffer jupyter)
+(a/defalias jupyter/server-list-kernels jupyter-server-list-kernels jupyter)
+(a/defalias jupyter/repl-restart-kernel jupyter-repl-restart-kernel jupyter)
 
 ;;* utils
   ;;* toggle
-(a/defalias toggle/truncate-lines toggle-truncate-lines toggle)
+(a/defalias toggle/truncate-wrap-lines toggle-truncate-lines toggle)
 
   ;;* bookmark
